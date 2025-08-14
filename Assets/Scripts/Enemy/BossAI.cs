fa835025d791261d3e2067eb1c94ab1fa5bd508d
Tyler <tylersmall2012@gmail.com>
2025-08-14 14:25:16 +0900

    Created Boss First attack. Missile's coming from the side in two's.

--- Assets/Scripts/Enemy/BossAI.cs
+++ Assets/Scripts/Enemy/BossAI.cs
@@ -9,10 +9,15 @@ public class BossAI : MonoBehaviour
     //health
     [SerializeField] private int _health = 100;
     private bool _isDead = false;
+    private bool _attackFinished;
     //bool to check if can move
     [SerializeField] private bool _canMove = true;
     [SerializeField] private bool _moveRight = true;
     [SerializeField] private GameObject[] _wayPoints;
+
+
+    [SerializeField] private GameObject[] _missleSpawnPoints;
+    [SerializeField] private GameObject _missilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +28,8 @@ public class BossAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-
         Movement();
     }
-
-
 
     private void Movement()
     {
@@ -41,9 +42,6 @@ public class BossAI : MonoBehaviour
 
             ChangeDirection();
         }
-
-        //if can move
-        //move left to edge of scene, then back right. Repeat.
     }
 
     private void ChangeDirection()
@@ -67,16 +65,73 @@ public class BossAI : MonoBehaviour
             float randomCoolDown = Random.Range(5f, 20f);
             yield return new WaitForSeconds(randomCoolDown);
             _canMove = false;
+            _attackFinished = false;
             //Fire an attack
+            Attack();
             Debug.Log("Fire!!");
-            yield return new WaitForSeconds(5f);
+            //yield return new WaitForSeconds(5f);
+            yield return new WaitUntil(() => _attackFinished);
             _canMove = true;
         }
     }
+
+    private void Attack()
+    {
+        int randomAttack = 0;//Random.Range(0, 5);
+        switch (randomAttack)
+        {
+            case 0://4 milssile's from the sides, 2 from each side
+                //pick two from left 0 - 3
+                int rightPoint = Random.Range(0, 2);   
+                int leftPoint = Random.Range(4, 6);
+                StartCoroutine(MissileAttackRoutine(leftPoint,rightPoint));
+                //pick two from right 4 - 7
+                //Instantiate left 
+                //instantiate right after 1.5 seconds -> IEnumerator?
+                break;
+            case 1://spawn enemies from top or side
+                break;
+            case 2://Lasers
+                break;
+            case 3://drop down bomb's to explode
+                break;
+            case 4://
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void Damage()
+    {
+        _health--;
+        //if health is half active 2 shields.
+        if (_health <= 0)
+        {
+            //show explosion animation
+            //destroy Boss
+        }
+    }
+
+    IEnumerator MissileAttackRoutine(int leftPoints, int rightPoints)
+    {
+        //display warning
+        Debug.Log($"Left points: {leftPoints} : {leftPoints + 2}. Right Points: {rightPoints} : {rightPoints +2}");
+        yield return new WaitForSeconds(1f);
+        GameObject rightMissile1 = Instantiate(_missilePrefab, _missleSpawnPoints[rightPoints].transform.position, Quaternion.Euler(Vector3.forward * 90));
+        rightMissile1.GetComponent<Missile>().MissileMoveLeft(false);
+        GameObject rightMissile2 = Instantiate(_missilePrefab, _missleSpawnPoints[rightPoints + 2].transform.position, Quaternion.Euler(Vector3.forward * 90));
+        rightMissile2.GetComponent<Missile>().MissileMoveLeft(false);
+        yield return new WaitForSeconds(1.5f);
+        GameObject leftMissile1 = Instantiate(_missilePrefab, _missleSpawnPoints[leftPoints].transform.position, Quaternion.Euler(Vector3.forward * -90));
+        leftMissile1.GetComponent<Missile>().MissileMoveLeft(true);
+        GameObject leftMissile2 = Instantiate(_missilePrefab, _missleSpawnPoints[leftPoints + 2].transform.position, Quaternion.Euler(Vector3.forward * -90));
+        leftMissile2.GetComponent<Missile>().MissileMoveLeft(true);
+        yield return new WaitForSeconds(3f);
+        _attackFinished = true;
+    }
 }
-
-
-
 /*
  * Boss moves back and forth and will randomly stop fire an attack.
  * at Half health 2 shields with heal will appear around Boss
---
+++ Assets/Scripts/Enemy/Missile.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    [SerializeField] private float _speed = 4f;
+    private bool _moveLeft = false;
+
+    private void Start()
+    {
+        Destroy(this.gameObject, 3f);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector2.up * _speed * Time.deltaTime);
+    }
+
+    public void MissileMoveLeft(bool moveLeft)
+        { _moveLeft = moveLeft; }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Player player = collision.gameObject.GetComponent<Player>();
+            if (player != null)
+            {
+                player.Damage();
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
